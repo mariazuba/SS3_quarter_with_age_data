ccd8a40f361f1b6a243bbadfae53bb909ba593b9
mariazuba <mariazuba@gmail.com>
2024-08-16 19:13:49 +0200

    model, output, report

---
+++ model_01_run.R
@@ -0,0 +1,40 @@
+## Run analysis, write model results
+
+## Before:
+## After:
+# libraries
+library(r4ss) 
+library(reshape2)
+library(readxl)
+library(openxlsx)
+library(dplyr)
+library(kableExtra)
+library(devtools)
+library(ss3diags)
+library(icesTAF)
+
+old_wd <- getwd()
+
+mkdir("model/run")
+
+path_esc<-"model/run"
+
+
+# directorios ----
+path.data<-"boot/data/Scenarios" 
+list.files(path.data)
+
+# Scenarios ----
+#'*------------------------------------------------------------------------------------------*
+### S0 ----
+s0_path   <- file.path(path.data, "S0") 
+s0_model  <- file.path(path_esc)
+cp(s0_path, s0_model)
+cp("boot/software/ss3", "model/run/S0")
+wd <- paste0(getwd(),"/model/run/S0")
+#setwd(wd)
+system(wd)
+system(paste0("chmod 755 ",wd,"/ss3"))
+r4ss::run(dir=wd, exe="ss3", skipfinished=FALSE, show_in_console =T)
+setwd(old_wd)
+
---
+++ report_01_run.R
@@ -0,0 +1,217 @@
+## Prepare plots and tables for report
+
+## Before:
+## After:
+rm(list=ls())
+
+
+# Load packages -----------------------------------------------------------
+
+library(icesTAF)
+library(r4ss)
+library(tidyverse)
+library(lubridate)
+library(ggpubr)
+library(ss3diags)
+library(flextable)
+
+
+# working directory
+wd <- getwd()
+# Load data ---------------------------------------------------------------
+
+# input data
+inputs<-"data/run"
+outputs<-"output/run"
+esc<-list.files(inputs)
+
+for(i in 1:length(esc)){
+  
+  load(paste0("output/run/",esc[i],"/output.RData"))
+  load(paste0("data/run/",esc[i],"/inputs.RData")) 
+  
+  mkdir(paste0("report/run/",esc[i]))
+  path<-paste0("report/run/",esc[i])
+
+# Figures --------------------------------------------
+## Temporal coverage of input data ----
+  png(file.path(paste0(path,"/input_data.png")),width=9,height=5,res=300,units='in')
+  sspar(mfrow = c(1, 1), plot.cex = 0.8)
+  SSplotData(output, subplots = 2,cex.main = 0.8,cex = 1,margins = c(2.1, 2.1, 1.1, 8.1))
+  dev.off()
+
+## Growth curve, length-weight relationship and maturity ----
+  png(file.path(paste0(path,"/Biology.png")),width=10,height=8,res=300,units='in')
+  sspar(mfrow = c(2, 2), plot.cex = 0.8)
+  SSplotBiology(output, subplot = c(1,5,6),seas=4,mainTitle = FALSE)
+  dev.off()
+
+## Fit data: Abundance indices ----
+  png(file.path(paste0(path,"/Indices_fit.png")),width=6,height=7,res=300,units='in')
+  sspar(mfrow = c(4, 2), plot.cex = 0.6)
+  SSplotIndices(output, subplots = c(2,3),mainTitle = T)
+  dev.off()
+
+
+## Fit data: Length composition (aggregated) ----
+  png(file.path(paste0(path,"/Age_fit_agg.png")),width=8,height=9,res=300,units='in')
+  SSplotComps(output, subplots = c(21),kind = "AGE",maxrows = 2,maxcols = 2,
+              showsampsize = F,showeffN = F,mainTitle = T)
+  dev.off()
+
+
+## Fit data: Length composition by source data ----
+### *FLEET by quarters* ----
+  png(file.path(paste0(path,"/Age_fit_Seine.png")),width=10,height=9,res=300,units='in')
+  SSplotComps(output, subplots = c(1),kind = "AGE",fleets = 1,maxrows = 12,maxcols =12,
+              showsampsize = F,showeffN = F,mainTitle = T)
+  dev.off()
+
+
+### *PELAGO spring survey* ----
+  png(file.path(paste0(path,"/Age_fit_Pelago.png")),width=8,height=9,res=300,units='in')
+  SSplotComps(output, subplots = c(1),kind = "AGE",fleets = 3,maxrows = 6,maxcols = 4,
+              showsampsize = F,showeffN = F,mainTitle = T)
+  dev.off()
+
+### *ECOCADIZ summer survey* ----
+  png(file.path(paste0(path,"/Age_fit_Ecocadiz.png")),width=8,height=9,res=300,units='in')
+  SSplotComps(output, subplots = c(1),kind = "AGE",fleets = 2,maxrows = 4,maxcols = 4,
+              showsampsize = F,showeffN = F,mainTitle = T)
+  dev.off()
+
+### *ECOCADIZ-RECLUTAS fall survey* ----
+
+  png(file.path(paste0(path,"/Age_fit_EcocadizRecl.png")),width=8,height=9,res=300,units='in')
+  SSplotComps(output, subplots = c(1),kind = "AGE",fleets = 5,maxrows = 4,maxcols = 4,
+              showsampsize = F,showeffN = F,mainTitle = T)
+  dev.off()
+
+## Residuals length composition by source data
+
+### *FLEET by quarters* ----
+  png(file.path(paste0(path,"/Age_residuals_Seine.png")),width=7,height=3,res=300,units='in')
+  SSplotComps(output, subplots = c(24),kind = "AGE",fleets = 1,maxrows = 12,maxcols = 5,
+              cexZ1 = 1.5,yupper=5,
+              cohortlines=T, showsampsize = F,showeffN = F)
+  dev.off()
+
+### *PELAGO spring survey* ----
+  png(file.path(paste0(path,"/Age_residuals_Pelago.png")),width=7,height=3,res=300,units='in')
+  SSplotComps(output, subplots = c(24),kind = "AGE",fleets =3,maxrows = 12,maxcols = 5,cexZ1 = 1.5,
+              showsampsize = F,showeffN = F)
+  dev.off()
+
+### *ECOCADIZ summer survey* ----
+  png(file.path(paste0(path,"/Age_residuals_Ecocadiz.png")),width=7,height=3,res=300,units='in')
+  SSplotComps(output, subplots = c(24),kind = "AGE",fleets = 2,maxrows = 12,maxcols = 5,cexZ1 = 1.5,
+              showsampsize = F,showeffN = F)
+  dev.off()
+
+### *ECOCADIZ-RECLUTAS fall survey* ----
+  png(file.path(paste0(path,"/Age_residuals_EcocadizRecl.png")),width=7,height=3,res=300,units='in')
+  SSplotComps(output, subplots = c(24),kind = "AGE",fleets = 5,maxrows = 12,maxcols = 5,cexZ1 = 1.5,
+              showsampsize = F,showeffN = F)
+  dev.off()
+
+## Run test indices ----
+  png(file.path(paste0(path,"/Runtest_residuals_indices.png")),width=7,height=7,res=300,units='in')
+  sspar(mfrow = c(3, 2), plot.cex = 0.8)
+  SSplotRunstest(output,subplots = "cpue", add = TRUE, legendcex = 0.8,verbose = F)
+  SSplotJABBAres(output,subplots = "cpue", add = TRUE, legendcex = 0.8,verbose = F)
+  dev.off()
+
+## Run test length ----
+  png(file.path(paste0(path,"/Runtest_residuals_age.png")),width=7,height=7,res=300,units='in')
+  sspar(mfrow = c(3, 2), plot.cex = 0.8)
+  SSplotRunstest(output,subplots = "age", add = TRUE, legendcex = 0.8,verbose = F)
+  SSplotJABBAres(output,subplots = "age", add = TRUE, legendcex = 0.8,verbose = F)
+  dev.off()
+
+## Selectivity ----
+  png(file.path(paste0(path,"/Selectividad.png")),width=6,height=5,res=300,units='in')
+  SSplotSelex(output,subplots =1)
+  dev.off()
+
+
+# tablas ----
+
+
+
+indices <- inputs$dat$CPUE%>% 
+  pivot_wider(
+    names_from = "index",  # Esta columna (index) se convertirá en nombres de columnas
+    values_from = c("obs", "se_log","seas")  # Estas columnas llenarán las nuevas columnas
+  )
+
+nsamp <- inputs$dat$agecomp %>%
+  filter(FltSvy >= 2) %>% 
+  select(Yr, FltSvy, Nsamp) %>% 
+  pivot_wider(names_from = FltSvy, values_from = Nsamp) %>% 
+  rename_with(~c("year", "nm_2", "nm_3", "nm_5"), .cols = c(Yr, `2`, `3`, `5`)) %>% mutate(nm_4=NA)
+
+combined_df <- left_join(indices, nsamp, by = "year")
+
+
+## table index by surveys ----
+ft1<-indices %>%
+  select(year,obs_2,obs_3,obs_4,obs_5) %>%
+  arrange(year) %>%  # Ordenar por la columna 'year'
+  mutate(across(c(obs_2,obs_3,obs_4,obs_5), \(x) round(x, 0))) %>%  # Redondear todas las columnas seleccionadas
+  flextable()  # Crear la flextable
+
+ft1<-set_header_labels(ft1, 
+                         year="year",
+                         obs_2="PELAGO",
+                         obs_3="ECOCADIZ",
+                         obs_4="BOCADEVA",
+                         obs_5="ECOCADIZ-RECLUTAS")
+
+ft1 <- add_header_row(ft1, 
+                      values = c("", "Acoustic Biomass (ton) by surveys"),
+                      colwidths = c(1, 4))
+
+ft1 <- colformat_double(ft1, digits=1, na_str = "")
+ft1 <- colformat_num(ft1,big.mark = "", na_str = "")
+ft1 <- align(ft1,part = "header", align = "center") 
+ft1 <- autofit(ft1)
+ft1
+
+# table data weighting by surveys ----
+ft2<-combined_df %>%
+  select(year,seas_2,se_log_2,nm_2,seas_3,se_log_3,nm_3,seas_4,se_log_4,nm_4,seas_5,se_log_5,nm_5) %>%
+  arrange(year)%>%
+  mutate(across(where(is.numeric), ~round(.x, 2))) %>%  # Redondear solo columnas numéricas
+  flextable()
+
+ft2<-set_header_labels(ft2,
+                       year="year",
+                       seas_2="month",
+                       se_log_2="cv",
+                       nm_2="nm",
+                       seas_3="month",
+                       se_log_3="cv",
+                       nm_3="nm",
+                       seas_4="month",
+                       se_log_4="cv",
+                       nm_4="nm",
+                       seas_5="month",
+                       se_log_5="cv",
+                       nm_5="nm")
+
+ft2 <- add_header_row(ft2, 
+                      values = c("", "PELAGO","ECOCADIZ","BOCADEVA","ECOCADIZ-RECLUTAS"),
+                      colwidths = c(1,3,3,3,3))
+
+ft2 <- colformat_double(ft2, digits=1, na_str = "")
+ft2 <- colformat_num(ft2,big.mark = "", na_str = "")
+ft2 <- align(ft2,part = "header", align = "center") 
+ft2 <- autofit(ft2)
+ft2
+
+
+save_as_image(ft1, path = paste0(path,"/tb_index.png"))
+save_as_image(ft2, path = paste0(path,"/tb_cv_nm.png"))
+save(ft1,ft2, file=paste0(path,"/tables_run.RData"))
+}
+
