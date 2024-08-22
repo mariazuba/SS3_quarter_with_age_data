df94d517e32efb4401f77fe011e56eecc2d07b55
mariazuba <mariazuba@gmail.com>
2024-08-22 11:10:38 +0200

    Actualizados output/run/S0 2024-08-22 11:10:38

--- output_01_run.R
+++ output_01_run.R
@@ -29,7 +29,7 @@ output <- r4ss::SS_output(dir = run.dir,forecast=FALSE)
 summary <- read.table(paste0(run.dir,"/ss_summary.sso"),header=F,sep="",na="NA",fill=T)
 
 mkdir(paste0("output/run/",esc[i]))
-run_out<<-paste0("output/run/",esc[i])
+run_out<-paste0("output/run/",esc[i])
 
 # Definir la ruta al directorio que quieres borrar
 dir_to_remove <- paste0("output/run/",esc[i],"/plots")
@@ -39,8 +39,24 @@ system(paste("rm -r", shQuote(dir_to_remove)))
 
 r4ss::SS_plots(replist = output, dir = run_out,
                printfolder = "plots",showpost = FALSE)
-
 # Write .RData ----
 save(output,  summary,         
      file=paste0(run_out,"/output.RData"))
+}
+
+# Se hace commit y push de los cambios 
+for(i in 1:length(esc)){
+  run_out <- paste0("output/run/",esc[i])
+# Agregar todos los archivos en la carpeta específica al área de preparación
+system2("git", args = c("add",run_out))
+system2("git", args = c("add","output_01_run.R"))
+# Realizar el commit con un mensaje descriptivo
+fecha_hora <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+commit_message <- paste0("Actualizados output/run/", esc[i]," ",fecha_hora)
+# Usar shQuote para manejar correctamente los espacios en el mensaje de commit
+commit_message_quoted <- shQuote(commit_message)
+# Ejecutar el comando git commit
+system2("git", args = c("commit", "-m", commit_message_quoted), stdout = TRUE, stderr = TRUE)
+# (Opcional) Subir los cambios al repositorio remoto
+system2("git", args = c("push"))
 }
