c7318539e45b95699d6f888d0917a03f45e0c5a9
mariazuba <mariazuba@gmail.com>
2024-08-22 11:09:06 +0200

    Actualizados model/run/S0 2024-08-22 11:09:06

--- model_01_run.R
+++ model_01_run.R
@@ -20,6 +20,7 @@ mkdir("model/run")
 path_esc<-"model/run"
 
 
+
 # directorios ----
 path.data<-"boot/data/run" 
 list.files(path.data)
@@ -38,3 +39,23 @@ system(paste0("chmod 755 ",wd,"/ss3"))
 r4ss::run(dir=wd, exe="ss3", skipfinished=FALSE, show_in_console =T)
 setwd(old_wd)
 
+
+# Se hace commit y push de los cambios cada vez que se ejecuta el modelo
+run_esc<-paste0(getwd(),"/model/run/")
+esc<-list.files(run_esc)
+
+for(i in 1:length(esc)){
+  run_mod <- paste0("model/run/",esc[i])
+  # Agregar todos los archivos en la carpeta específica al área de preparación
+  system2("git", args = c("add",run_mod))
+  system2("git", args = c("add","model_01_run.R"))
+  # Realizar el commit con un mensaje descriptivo
+  fecha_hora <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+  commit_message <- paste0("Actualizados model/run/", esc[i]," ", fecha_hora)
+  # Usar shQuote para manejar correctamente los espacios en el mensaje de commit
+  commit_message_quoted <- shQuote(commit_message)
+  # Ejecutar el comando git commit
+  system2("git", args = c("commit", "-m", commit_message_quoted), stdout = TRUE, stderr = TRUE)
+  # (Opcional) Subir los cambios al repositorio remoto
+  system2("git", args = c("push"))
+}
