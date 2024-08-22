29089f962764c8e54a456fe4ea9f2608a0fcad1e
mariazuba <mariazuba@gmail.com>
2024-08-22 16:34:40 +0200

    Actualizados output/run/S0 2024-08-22 16:34:40

--- output_01_run.R
+++ output_01_run.R
@@ -42,9 +42,9 @@ run.dir  <- paste0(run_esc,esc[i])
 output <- r4ss::SS_output(dir = run.dir,forecast=FALSE)
 summary <- read.table(paste0(run.dir,"/ss_summary.sso"),header=F,sep="",na="NA",fill=T)
 
-# setting recruitment
+# setting 
 R0 <- output$estimated_non_dev_parameters["SR_LN(R0)", "Value"]
-sigmaR<-inputs$ctl$SR_parms["SR_sigmaR", "INIT"]
+
 
 
 mkdir(paste0("output/run/",esc[i]))
@@ -59,7 +59,7 @@ system(paste("rm -r", shQuote(dir_to_remove)))
 r4ss::SS_plots(replist = output, dir = run_out,
                printfolder = "plots",showpost = FALSE)
 # Write .RData ----
-save(output,  summary, R0,  sigmaR,      
+save(output,  summary, R0,      
      file=paste0(run_out,"/output.RData"))
 }
 
