64a400efd9a0309a7dc506f31053efe157bf1b77
mariazuba <mariazuba@gmail.com>
2024-08-28 11:44:59 +0200

    Actualiza boot2024-08-28 11:44:59

--- bootstrap.R
+++ bootstrap.R
@@ -6,10 +6,6 @@
 # and software into the `boot/data` and `boot/software` directories using `taf.bootstrap()`. 
 # Finally, it commits and pushes these changes to the Git repository, 
 # ensuring proper version control of the generated files. 
-#'*To avoid making changes directly to the main repository,*
-#'* it is recommended to either comment out the section that performs the commit and push, *
-#'* or switch to a different branch before running the script. *
-#'* This will help ensure proper version control of the generated files without impacting the main branch.*
 
 library("icesTAF")
 library("r4ss")
@@ -36,9 +32,4 @@ r4ss::get_ss3_exe(dir = "boot/initial/software", version = "v3.30.22.1")
 draft.software('boot/initial/software/ss3',file=TRUE)
 taf.bootstrap() # se obtiene los datos de la carpeta boot/data, 
 
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> ef8f9f60e4a6d8900313a05611691de3aec307e8
